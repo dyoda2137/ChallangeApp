169810b543a27281481a6e1019bf03313c7836f1
dyoda2137 <sting.lucas@gmail.com>
2024-05-09 22:56:55 +0200

    dzień 4

--- ChallangeApp/Program.cs
+++ ChallangeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Tomek";
+var age = 30;
+var sex = "men";
+
+if (sex == "woman" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa lat 30");
+}
+else if (sex == "men" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
