8d36e1446d4e8b595e7e3c3c6dd996c9bf71603e
dyoda2137 <sting.lucas@gmail.com>
2024-05-12 16:46:18 +0200

    dzień 7

---
+++ ChallangeApp/ChallangeApp.Tests/EmployeeTests.cs
@@ -0,0 +1,53 @@
+namespace ChallangeApp.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void WhenEmployee1CollectScore_ThenCorrectResult()
+        {
+            // arrange
+            Employee employee1 = new Employee("Adam","¯yniec",33);
+            employee1.AddScore(5);
+            employee1.AddScore(7);
+            employee1.AddScore(-2);
+            
+            //act
+            var result = employee1.Result;
+
+            // assert
+            Assert.AreEqual(10, result);
+        }
+
+        [Test]
+        public void WhenEmployee2CollectScore_ThenCorrectResult()
+        {
+            // arrange
+            Employee employee2 = new Employee("Monika", "Tomaszewska", 25);
+            employee2.AddScore(9);
+            employee2.AddScore(-5);
+            employee2.AddScore(7);
+
+            //act
+            var result = employee2.Result;
+
+            // assert
+            Assert.AreEqual(11, result);
+        }
+
+        [Test]
+        public void WhenEmployee3CollectScore_ThenCorrectResult()
+        {
+            // arrange
+            Employee employee3 = new Employee("Mateusz", "Buga³a", 28);
+            employee3.AddScore(7);
+            employee3.AddScore(-1);
+            employee3.AddScore(6);
+
+            //act
+            var result = employee3.Result;
+
+            // assert
+            Assert.AreEqual(12, result);
+        }
+    }
+}
